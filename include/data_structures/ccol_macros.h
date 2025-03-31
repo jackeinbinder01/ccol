82e14c917e8693cd17762061a44b48903476d254
Jack Einbinder <jackeinbinder01@gmail.com>
2025-03-31 01:37:33 -0400

    iter

--- include/data_structures/ccol_macros.h
+++ include/data_structures/ccol_macros.h
@@ -10,10 +10,12 @@
 #ifndef CCOL_MACROS_H
 #define CCOL_MACROS_H
 
-#define ITER_FOR(type, var, iter_ptr)                         \
-    for (type* var = NULL;                                    \
-        (iter_ptr)->has_next(iter_ptr) &&                     \
-        ((var = (type*)iter_ptr->next(iter_ptr)) || 1);)
+#define ITER_FOR(type, var, iter_ptr)                                   \
+    for (iterator_t* _it = (iter_ptr); _it; _it = NULL)                 \
+        for (type* var = NULL;                                          \
+            _it->has_next(_it) &&                                       \
+            ((var = (type*)_it->next(_it)) || 1);)                      \
+            for (int done = (_it->destroy(_it), 0); !done; done = 1)
 
 
 #endif //CCOL_MACROS_H
